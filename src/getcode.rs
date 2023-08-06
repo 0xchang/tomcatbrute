a6b59edccdeaffd59659f5915141d82827a8744b
0xchang <oxcahng@163.com>
2023-08-06 20:40:36 +0800

    src

--- src/getcode.rs
+++ src/getcode.rs
@@ -1,7 +1,8 @@
-use reqwest::{header::HeaderMap, Client};
+use reqwest::{header::{HeaderMap}, Client};
 use std::{sync::Arc, time::Duration};
+use reqwest::header::HeaderValue;
 
-pub async fn get_status_code(url: Arc<String>, up_en_b64: &str) -> u16 {
+pub async fn get_status_code(url: Arc<String>, up_en_b64: &str) -> (u16, Option<HeaderValue>) {
     let mut headers = HeaderMap::new();
     headers.insert("User-Agent",
      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36".parse().unwrap());
@@ -15,22 +16,39 @@ pub async fn get_status_code(url: Arc<String>, up_en_b64: &str) -> u16 {
         .build()
         .unwrap();
     let resp = c.get(&*url).send().await;
-    let code = match resp {
-        Ok(r) => r.status().into(),
-        Err(_) => 0,
+
+    let resp = match resp {
+        Ok(r)=>Some(r),
+        Err(_)=>None
     };
-    code
+    let resp = match resp {
+        Some(r)=>{
+            r
+        },
+        None=>todo!()
+    };
+
+    let mut cookies=None;
+    let mut code=0;
+
+    if let Some(cookie) = resp.headers().get("Set-Cookie") {
+        cookies = cookie.clone().into();
+    };
+    if let Some(codes) = resp.status().into() {
+        code = codes.into();
+    }
+    (code,cookies)
 }
 
-pub async fn check_code(code: u16, up: &str) -> Option<&str> {
+pub async fn check_code(result: (u16, Option<HeaderValue>), up: &str) -> Option<&str> {
+    let (code,cookies) = result;
     let res = if code == 200 || code == 302 {
-        println!("code is {},success: {}", code, up);
+        println!("code is {}, success: {} , Cookie: {:#?}", code, up , cookies);
         Some(up)
     } else if code == 403 || code == 404 {
         println!("code is {}, stop...", code);
-        None
+        std::process::exit(0);
     } else if code == 401 {
-        println!("code is 401, wait...");
         None
     } else {
         None
